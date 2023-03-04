74463baaf7062830bb38e45954f6932371d2b389
kahboom <rachelyordan@gmail.com>
2023-03-04 18:50:22 +0000

    fix: fix issue with nested anchor

--- src/components/elements/BlogSidebar.tsx
+++ src/components/elements/BlogSidebar.tsx
@@ -37,7 +37,7 @@ const BlogSidebar = ({ categories, configSidebar, show, tags, trendingPosts }: I
               {categories.map((category, idx) => {
                 return (
                   <li className={'cat-item cat-item-' + idx} key={idx}>
-                    <Link href={`/category/${category.value}`}>{category.label}</Link> {/*(3)*/}
+                    <Link href={`/category/${category.value}`}>{category.label}</Link>
                   </li>
                 );
               })}
@@ -65,26 +65,22 @@ const BlogSidebar = ({ categories, configSidebar, show, tags, trendingPosts }: I
                   <div className="post-thumb d-flex border-radius-5 img-hover-scale mb-15">
                     <Link href={`/${post.slug}`}>
                       <div style={{ width: '100%' }}>
-                        <a>
-                          <Image
-                            src={post.image as string}
-                            alt={post.imageAlt ?? ''}
-                            sizes="100vw"
-                            style={{ width: '100%', height: 'auto' }}
-                            width={post.imageOriginalWidth ?? 600}
-                            height={post.imageOriginalHeight ?? 400}
-                          />
-                        </a>
+                        <Image
+                          src={post.image as string}
+                          alt={post.imageAlt ?? ''}
+                          sizes="100vw"
+                          style={{ width: '100%', height: 'auto' }}
+                          width={post.imageOriginalWidth ?? 600}
+                          height={post.imageOriginalHeight ?? 400}
+                        />
                       </div>
                     </Link>
                   </div>
                   <div className="post-content media-body">
                     <h5 className="post-title mb-10 text-limit-2-row">
                       <Link href={`/${post.slug}`}>
-                        <>
-                          <a>{post.title}</a>
-                          {post.draft && <DraftBadge />}
-                        </>
+                        {post.title}
+                        {post.draft && <DraftBadge />}
                       </Link>
                     </h5>
                     {/*<div className='entry-meta meta-13 font-xxs color-grey'>*/}
