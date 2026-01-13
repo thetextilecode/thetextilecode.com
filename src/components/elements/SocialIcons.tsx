1d1091dd7f7795aa66d42faba2af6b6be7ac39aa
kahboom <rachelyordan@gmail.com>
2026-01-13 21:21:51 +0000

    fix: update rel attributes for social icons and footer links to enhance security

--- src/components/elements/SocialIcons.tsx
+++ src/components/elements/SocialIcons.tsx
@@ -10,19 +10,19 @@ const SocialIcons = ({ facebook, instagram, linkedIn, pinterest, twitter }: ISoc
   return (
     <div className="mobile-social-icon wow fadeIn animated mb-sm-5 mb-md-0 animated">
       {facebook && (
-        <a href={`https://facebook.com/${facebook}`} rel={'nofollow noreferrer'} target={'_blank'}>
+        <a href={`https://facebook.com/${facebook}`} rel={'nofollow noopener noreferrer'} target={'_blank'}>
           <img src="/assets/images/theme/icons/icon-facebook.svg" alt="Facebook" />
         </a>
       )}
       {twitter && (
-        <a href={`https://twitter.com/${twitter}`} rel={'nofollow noreferrer'} target={'_blank'}>
+        <a href={`https://twitter.com/${twitter}`} rel={'nofollow noopener noreferrer'} target={'_blank'}>
           <img src="/assets/images/theme/icons/icon-twitter.svg" alt="Twitter" />
         </a>
       )}
       {instagram && (
         <a
           href={`https://instagram.com/${instagram}`}
-          rel={'nofollow noreferrer'}
+          rel={'nofollow noopener noreferrer'}
           target={'_blank'}
         >
           <img src="/assets/images/theme/icons/icon-instagram.svg" alt="Instagram" />
@@ -31,7 +31,7 @@ const SocialIcons = ({ facebook, instagram, linkedIn, pinterest, twitter }: ISoc
       {pinterest && (
         <a
           href={`https://pinterest.com/${pinterest}`}
-          rel={'nofollow noreferrer'}
+          rel={'nofollow noopener noreferrer'}
           target={'_blank'}
         >
           <img src="/assets/images/theme/icons/icon-pinterest.svg" alt="Pinterest" />
